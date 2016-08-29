4c4f12dd087e60ee9d58613c0fce3503ec7d13be
Mathias Neerup <manee12@student.sdu.dk>
2016-08-29 13:42:28 +0200

    Fixing typo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ plugin_package = "octoprint_autofanon"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "AutoFan On"
+plugin_name = "AutoFanOn"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.0.0"
