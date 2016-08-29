adf39bc1e102a0b9b50711c72b3ee3a2283bbeba
Mathias Neerup <manee12@student.sdu.dk>
2016-08-29 17:48:55 +0200

    Implementing dynamic temperature threshold

--- octoprint_autofanon/__init__.py
+++ octoprint_autofanon/__init__.py
@@ -1,27 +1,50 @@
 # coding=utf-8
 
+from __future__ import absolute_import
+
 import octoprint.plugin
+from octoprint.util import RepeatedTimer
 
-class AutoFanOnPlugin(octoprint.plugin.OctoPrintPlugin):
-	def on_gcode(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
-		self._logger.info("Received code: %s", gcode);
-		if gcode and gcode == "M109":
-			cmd = "M106"
-		return cmd,
+class AutoFanOnPlugin(octoprint.plugin.StartupPlugin,
+		octoprint.plugin.OctoPrintPlugin,
+		octoprint.plugin.TemplatePlugin,
+		octoprint.plugin.SettingsPlugin):
 
-#	def sent_m106(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
-#		if gcode and gcode == "M106":
-#			self._logger.info("Just sent M106: {cmd}".format(**locals()))
+        def __init__(self):
+                self._checkTempTimer = None
+                self._interval = 5 # [secs]
+
+        def on_after_startup(self):
+                self._checkTempTimer = RepeatedTimer(self._interval, self.checkTemp, None, None, True)
+                self._checkTempTimer.start()
+
+                self._threshold = self._settings.get(["temp_threshold"]
+
+        def checkTemp(self):
+                temp_hotend = 0
+                try:
+                        temp_hotend = self._printer.get_current_temperatures()["tool0"]["actual"]
+                except Exception,e:
+                        self._logger.info("Not ready.. Exception: %s", str(e));
+
+                if temp_hotend >= self._threshold:
+                        self._printer.commands(["M106"])
+
+
+        def get_settings_defaults(self):
+                return dict(temp_threshold=50)
+
+	def get_template_vars(self):
+	        return dict(temp_threshold=self._settings.get(["temp_threshold"]))
+
+	def get_template_configs(self):
+		return [
+			dict(type="settings", custom_bindings=False)
+		]
 
 __plugin_name__ = "AutoFanOn"
 
+
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = AutoFanOnPlugin()
-
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.on_gcode,
-#		"octoprint.comm.protocol.gcode.sent": __plugin_implementation__.sent_m106
-	}
-
+        global __plugin_implementation__
+        __plugin_implementation__ = AutoFanOnPlugin()
